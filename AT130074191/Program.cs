770168b05e4bdd2f456ea0d7249540609bb9259c
30074191 <30074191@DELL603931>
2024-02-05 11:14:09 +0800

    Add project files.

---
+++ AT130074191/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT130074191
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
